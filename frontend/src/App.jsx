2e71894368a04c51510827c10322abb24a8de21a
ayush-maurya-19 <ayushmaurya1901@gmail.com>
2023-09-23 21:27:01 +0530

    components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import Home from "./components/Home";
+import Login from "./components/Login";
+import Signup from "./components/Signup";
+import AddRentSpace from "./components/AddRentSpace";
+import ManageRentSpaces from "./components/ManageRentSpaces";
+import SpaceDetails from "./components/SpaceDetails";
+import BrowserRentSpace from "./components/BrowserRentSpace";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div>
-
+      <BrowserRouter>
+      <Navbar>
+        <Routes>
+          <Route element={<Home />} path="/" />
+          <Route element={<Login />} path="/login" />
+          <Route element={<Signup />} path="/signup" />
+          <Route element={<AddRentSpace />} path="/addrentspace" />
+          <Route element={<BrowserRentSpace />} path="/browserrentspace" />
+          <Route element={<ManageRentSpaces />} path="/ManageRentSpaces" />
+          <Route element={<SpaceDetails />} path="/spacedetails" />
+        </Routes>
+      </Navbar>        
+      </BrowserRouter>
     </div>
   );
 }
